22de308a6446373435bb313f1d05e4a2c9b85809
Andressa <desasilva@outlook.com>
2018-10-30 08:14:47 -0300

    Adicionando_paginas

--- site/projectLms/Coordenador/views.py
+++ site/projectLms/Coordenador/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def portalCoordenador(request):
     return render(request, 'portalCoordenador.html')
+
+def avisos(request):
+    return render(request, 'avisos.html')
+
+def ementas(request):
+    return render(request, 'ementas.html')
+
+def gruposalunos(request):
+    return render(request, 'gruposalunos.html')
+
+def planoensino(request):
+    return render(request, 'planoensino.html')
